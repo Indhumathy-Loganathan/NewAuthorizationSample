3fde9b1293991a880e6cc5011d5222c7cc61bd75
AzureAD\IndhumathyLoganathan <indhumathy.loganathan@syncfusion.com>
2022-12-29 18:09:46 +0530

    Sample updated.

---
+++ Controller/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NewAuthorizationSample.Controller
+{
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("PostCount")]
+        public IActionResult PostCount(string item)
+        {
+            return View();
+        }
+
+        private IActionResult View()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
